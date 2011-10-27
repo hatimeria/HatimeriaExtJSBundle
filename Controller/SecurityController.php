a8cbb49214c71127ddb8fb23d1f28e9d71443c33
Michał Wujas <michal@wujas.pl>
2011-10-27 10:29:45 +0200

    ajax login implementation

---
+++ Controller/SecurityController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Hatimeria\ExtJSBundle\Controller;
+
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Hatimeria\ExtJSBundle\Response\Success;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Template()
+     */
+    public function loginAction()
+    {
+        // code from fos user security controller
+        $request = $this->container->get('request');
+        /* @var $request \Symfony\Component\HttpFoundation\Request */
+        $session = $request->getSession();
+        /* @var $session \Symfony\Component\HttpFoundation\Session */
+
+        // get the error if any (works with forward and redirect -- see below)
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } elseif (null !== $session && $session->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        if ($error) {
+            $failure = new Failure("Nieprawidłowy login lub hasło");
+            $content = json_encode($failure->toArray());
+            
+            return new Response($content);
+        }
+        
+        return array();
+    }
+    
+    public function afterLoginAction()
+    {
+        $success = new Success();
+        
+        $content = json_encode($success->toArray());
+        
+        return new Response($content);
+    }
+}
