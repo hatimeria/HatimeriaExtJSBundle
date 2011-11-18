a8975a3ce8778ca3b65a47d532e0453b142c23f8
Michał Wujas <michal@wujas.pl>
2011-11-18 17:46:59 +0100

    fixes new annotation system

--- Annotation/Remote.php
+++ Annotation/Remote.php
@@ -2,6 +2,13 @@
 
 namespace Hatimeria\ExtJSBundle\Annotation;
 
+/**
+ * Represents a @Remote annotation.
+ *
+ * @Annotation
+ * @Target("METHOD")
+ * 
+ */
 class Remote
 {
     public function __construct()
