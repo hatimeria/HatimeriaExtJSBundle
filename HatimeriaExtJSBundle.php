2f7c05b7ceb907c415bb52c8a06ee54f7245aeab
freakphp <me@freakphp.com>
2011-12-06 10:45:47 +0100

    Add mappings provider interface and tag compiler (hatimeria_extjs.mappings)

--- HatimeriaExtJSBundle.php
+++ HatimeriaExtJSBundle.php
@@ -2,8 +2,18 @@
 
 namespace Hatimeria\ExtJSBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle,
+    Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use Hatimeria\ExtJSBundle\DependencyInjection\Compiler\RegisterMappingsPass;
 
 class HatimeriaExtJSBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new RegisterMappingsPass());
+    }
+
 }
